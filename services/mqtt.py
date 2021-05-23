dd3ef7f4f4d8d0d54f0f96bd6c8207ba16ae7aaf
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-22 20:30:14 -0700

    Add sleep for testing succesful connection

--- services/mqtt.py
+++ services/mqtt.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 import os
+import time
 
 def load_env():
   load_dotenv()
@@ -22,8 +23,10 @@ def get_connection():
   host = os.environ.get("MQTT_BROKER_HOST")
   
   client = get_client()
-  client.on_connect=onconnect
+  client.on_connect=on_connect
   client.username_pw_set(username, password)
   client.connect(host)
+  client.loop_start()  #Start loop 
+  time.sleep(4)
 
   return client
