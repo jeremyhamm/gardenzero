7fd1c81b830add4cd6a99cab161cf239be7c81e0
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-23 13:25:54 -0700

    Add heartbeat service for determining if the system is operational

---
+++ services/heartbeat.py
@@ -0,0 +1,8 @@
+import time
+sys.path.append(os.path.abspath('../services'))
+import mqtt
+
+client = mqtt.get_connection()
+while True:
+  client.publish("garden/heartbeat", True)
+  time.sleep(300)
