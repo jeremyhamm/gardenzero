08496d2079e59dbc9d692a8f11ce5c8ffa7a493c
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-23 17:19:10 -0700

    Set initial mode to watered

--- sensors/soil-moisture.py
+++ sensors/soil-moisture.py
@@ -23,13 +23,14 @@ def callback(channel):
 
 # MQTT
 client = mqtt.get_connection()
+client.publish("garden/soil-moisture", True) # Set initial mode to watered
 
 #GPIO SETUP
 channel = int(os.environ.get("SOIL_MOISTURE_GPIO"))
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
 
 
 while True:
