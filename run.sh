32f243fdfe95a4f49a159414a6ffe28776d2518a
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-23 21:49:42 -0700

    Fix syntax error in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Sensors
-nohup /home/pi/apps/garden-zero/sensors/soil-temperature.py &
-nohup /home/pi/apps/garden-zero/sensors/soil-moisture.py &
+nohup python3 /home/pi/apps/garden-zero/sensors/soil-temperature.py &
+nohup python3 /home/pi/apps/garden-zero/sensors/soil-moisture.py &
 
 # Metrics
-nohup /home/pi/apps/garden-zero/metrics/heartbeat.py
+nohup python3 /home/pi/apps/garden-zero/metrics/heartbeat.py &
