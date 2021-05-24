afa1137ca97c6f32ef1d43ad6dee366f62fcd8f5
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-23 19:01:33 -0700

    Update pub message for soil moisture to str

--- sensors/soil-moisture.py
+++ sensors/soil-moisture.py
@@ -16,10 +16,10 @@ import mqtt
 def callback(channel):
   if GPIO.input(channel):
     print("LED off")
-    client.publish("garden/soil-moisture", {'need-water': True})
+    client.publish("garden/soil-moisture", 'need-water')
   else:
     print("LED on")
-    client.publish("garden/soil-moisture", {'need-water': False})
+    client.publish("garden/soil-moisture", 'watered')
 
 # MQTT
 client = mqtt.get_connection()
