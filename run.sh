4ac4d504840962ae5912796c8b5a618b97e70598
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-23 21:40:16 -0700

    Update run.sh with nohup

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Sensors
-python3  /home/pi/apps/garden-zero/sensors/soil-temperature.py &
-python3  /home/pi/apps/garden-zero/sensors/soil-moisture.py &
+nohup /home/pi/apps/garden-zero/sensors/soil-temperature.py &
+nohup /home/pi/apps/garden-zero/sensors/soil-moisture.py &
 
 # Metrics
-python3  /home/pi/apps/garden-zero/metrics/heartbeat.py
+nohup /home/pi/apps/garden-zero/metrics/heartbeat.py
