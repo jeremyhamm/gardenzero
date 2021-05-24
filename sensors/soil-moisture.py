d4ee22a8b38805a6494d8a7b1f6e1542ccd88804
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-23 17:17:37 -0700

    Update logic for soil-moisture sensor

--- sensors/soil-moisture.py
+++ sensors/soil-moisture.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 """
 Measure soil moisture with the Kuman soil moisture sensor
 https://www.amazon.com/gp/product/B071F4RDHY/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=piddlerinther-20&linkId=77f1c0f9c67c51d76b687628afa62ce1&language=en_US
@@ -6,10 +8,10 @@ https://www.amazon.com/gp/product/B071F4RDHY/ref=as_li_ss_tl?ie=UTF8&linkCode=sl
 import sys
 import os
 import time
+import RPi.GPIO as GPIO
 from dotenv import load_dotenv
 sys.path.append(os.path.abspath('../services'))
 import mqtt
-import RPi.GPIO as GPIO
 
 def callback(channel):
   if GPIO.input(channel):
@@ -19,14 +21,16 @@ def callback(channel):
     print("I NEED WATER!")
     client.publish("garden/soil-moisture", False)
 
+# MQTT
 client = mqtt.get_connection()
-GPIO.setmode(GPIO.BCM)
+
+#GPIO SETUP
 channel = int(os.environ.get("SOIL_MOISTURE_GPIO"))
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-GPIO.add_event_callback(channel, callback)
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 
 
 while True:
   time.sleep(1)
-
