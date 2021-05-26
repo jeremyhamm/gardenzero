1b0cba9c2511f356271eb21fe40281712ebc484e
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-25 20:02:20 -0700

    Remove sleep from run bash

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-(python3 /home/pi/apps/garden-zero/sensors/soil-temperature.py;python3 /home/pi/apps/garden-zero/sensors/soil-moisture.py;python3 /home/pi/apps/garden-zero/metrics/heartbeat.py)&
+# Sensors
+python3 /home/pi/apps/garden-zero/sensors/soil-temperature.py &
+python3 /home/pi/apps/garden-zero/sensors/soil-moisture.py &
+
+# Metrics
+python3 /home/pi/apps/garden-zero/metrics/heartbeat.py &
