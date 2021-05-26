87e9ab47f60b8e69ec2e6241f07f07a192e3dc47
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-25 19:04:50 -0700

    Update path to MQTT module

--- metrics/heartbeat.py
+++ metrics/heartbeat.py
@@ -8,7 +8,7 @@ https://projects.raspberrypi.org/en/projects/build-your-own-weather-station/2
 import sys
 import os
 import time
-sys.path.append(os.path.abspath('../services'))
+sys.path.append(os.path.abspath('./services'))
 import mqtt
 
 client = mqtt.get_connection()
