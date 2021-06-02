6b84a868addbf8ae0433e40dd2499a6c93d4d117
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-06-01 19:01:01 -0700

    Update humidity syntax

--- sensors/humidity.py
+++ sensors/humidity.py
@@ -21,12 +21,12 @@ result = instance.read()
 
 try:
   while True:
-	  result = instance.read()
-	  if result.is_valid():
+    result = instance.read()
+    if result.is_valid():
       print("Last valid input: %-3.1f" % str(datetime.datetime.now()))
       print("Temperature: %-3.1f C" % result.temperature)
       print("Humidity: %-3.1f %%" % result.humidity)
-	    time.sleep(6)
+      time.sleep(6)
 
 except KeyboardInterrupt:
     print("Cleanup")
