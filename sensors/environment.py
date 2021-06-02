50858baaf17b60e6dc0482f30ded071b0276693f
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-06-01 20:44:21 -0700

    Add seperate MQTT streams for temp and humidity

--- sensors/environment.py
+++ sensors/environment.py
@@ -11,7 +11,7 @@ import dht11
 import time
 import datetime
 from dotenv import load_dotenv
-sys.path.append(os.path.abspath('./services'))
+sys.path.append(os.path.abspath('../services'))
 import mqtt
 
 # initialize GPIO
@@ -38,7 +38,8 @@ try:
       humidity = formatHumidity(result.humidity)
       interval = int(os.environ.get("INTERVAL_SECONDS"))
 
-      client.publish("garden/environment", f"Temperature: {temp}\n Humidity: {humidity}")
+      client.publish("garden/temperature", f"Temperature: {temp}")
+      client.publish("garden/humidity", f"Humidity: {humidity}")
 
       time.sleep(interval)
 
