af67f924ecf93d4c7ada2fcc3e2e88601690355f
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-06-01 19:00:26 -0700

    Update humidity syntax

--- sensors/humidity.py
+++ sensors/humidity.py
@@ -7,6 +7,8 @@ https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-ras
 
 import RPi.GPIO as GPIO
 import dht11
+import time
+import datetime
 
 # initialize GPIO
 GPIO.setwarnings(True)
@@ -18,16 +20,14 @@ instance = dht11.DHT11(pin = 21)
 result = instance.read()
 
 try:
-	while True:
+  while True:
 	  result = instance.read()
 	  if result.is_valid():
-      print("Last valid input: " + str(datetime.datetime.now()))
-
+      print("Last valid input: %-3.1f" % str(datetime.datetime.now()))
       print("Temperature: %-3.1f C" % result.temperature)
       print("Humidity: %-3.1f %%" % result.humidity)
-
 	    time.sleep(6)
-      
+
 except KeyboardInterrupt:
     print("Cleanup")
     GPIO.cleanup()
