5cc0a40f32adb1d40119af098eb2880539f90574
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-06-01 18:37:22 -0700

    Update humidity sensor to use dht11 library

--- sensors/humidity.py
+++ sensors/humidity.py
@@ -6,58 +6,20 @@ https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-ras
 """
 
 import RPi.GPIO as GPIO
-import time
+import dht11
 
-data = []
-
-GPIOPin = 21
-
+# initialize GPIO
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIOPin,GPIO.OUT)
-GPIO.output(GPIOPin,GPIO.HIGH)
-time.sleep(0.025)
-GPIO.output(GPIOPin,GPIO.LOW)
-time.sleep(0.02)
-GPIO.setup(GPIOPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.cleanup()
 
-def bin2dec(string_num):
-  return str(int(string_num, 2))
+# read data using pin 14
+instance = dht11.DHT11(pin = 21)
+result = instance.read()
 
-for i in range(0,500):
-  data.append(GPIO.input(GPIOPin))
-
-
-bit_count = 0
-tmp = 0
-count = 0
-HumidityBit = ""
-crc = ""
-
-try:
-  for i in range(0, 8):
-    bit_count = 0
-
-    while data[count] == 0:
-      tmp = 1
-      count = count + 1
-
-    while data[count] == 1:
-      bit_count = bit_count + 1
-      count = count + 1
-
-    if bit_count > 3:
-      crc = crc + "1"
-    else:
-      crc = crc + "0"
-except:
-  print("ERR_RANGE")
-  exit(0)
-
-Humidity = bin2dec(HumidityBit)
-
-if int(Humidity) - int(bin2dec(crc)) == 0:
-  print(Humidity)
-
+if result.is_valid():
+  print("Temperature: %-3.1f C" % result.temperature)
+  print("Humidity: %-3.1f %%" % result.humidity)
 else:
-  print("ERR_CRC")
+  print("Error: %d" % result.error_code)
 
